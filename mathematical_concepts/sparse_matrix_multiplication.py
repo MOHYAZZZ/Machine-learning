505064378d85e52628b9a62ce1a4fc3174730cb7
MOHYAZZZ <mohammadreza.yazdian.2020@live.rhul.ac.uk>
2023-06-12 21:11:35 +0800

    This commit adds a Python function for efficiently multiplying two sparse matrices together. It takes advantage of sparsity to reduce computations and improves efficiency.

---
+++ mathematical_concepts/sparse_matrix_multiplication.py
@@ -0,0 +1,19 @@
+def sparse_matrix_multiplication(matrix_a, matrix_b):
+    # Check if matrices can be multiplied
+    if len(matrix_a) == 0 or len(matrix_b) == 0 or len(matrix_a[0]) != len(matrix_b):
+        return [[]]
+
+    # Initialize result matrix with appropriate dimensions
+    result = [[0] * len(matrix_b[0]) for _ in range(len(matrix_a))]
+
+    # Iterate over each non-zero element in matrix_a
+    for i in range(len(matrix_a)):
+        for k in range(len(matrix_a[0])):
+            if matrix_a[i][k] != 0:
+                # Iterate over corresponding row in matrix_b to find non-zero elements
+                for j in range(len(matrix_b[0])):
+                    if matrix_b[k][j] != 0:
+                        # Accumulate product of non-zero elements in the result matrix
+                        result[i][j] += matrix_a[i][k] * matrix_b[k][j]
+
+    return result
